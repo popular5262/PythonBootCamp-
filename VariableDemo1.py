85d6a84e28acbec4ffd364358648cfcbf69c5548
popular5262 <memipopular2@gmail.com>
2022-10-28 02:15:37 +0100

    hey Frank i added a new file valled variableDemo1.py check it out

---
+++ VariableDemo1.py
@@ -0,0 +1,31 @@
+
+coke_price = 2000
+fanta_price = 1000
+sprit_price = 5000
+ciroc_price = 4000
+hennessy_price = 5000
+moet_price = 6500
+pepsi_price = 2000
+merinda_price = 1500
+
+drink_price = coke_price + fanta_price + sprit_price + ciroc_price + hennessy_price + moet_price + pepsi_price + merinda_price
+print()
+print("coke_price", coke_price)
+print("fanta_price", fanta_price)
+print("sprit_price", sprit_price)
+print("ciroc_price", ciroc_price)
+print("hennessy_price", hennessy_price)
+print("moet_price", moet_price)
+print("pepsi_price", pepsi_price)
+print("merinda_price", merinda_price)
+
+print("total amount of coke_price plus fanta_price plus sprit_price plus ciroc_price plus hennessy_price plus moet_price plus pepsi_price plus merinda_price is: ", drink_price, "drinks")
+
+coke_price = 2000
+fanta_price = 1000
+sprit_price = 5000
+ciroc_price = 4000
+hennessy_price = 5000
+moet_price = 6500
+pepsi_price = 2000
+merinda_price = 1500
